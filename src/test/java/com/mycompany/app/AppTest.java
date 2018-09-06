bc11d5ba51a925dcbcd95636484d41f89cb959ad
Radek Hubner <radek.hubnar@digital.hmrc.gov.uk>
2018-09-06 22:25:37 +0100

    Add test which fail.

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -40,6 +40,11 @@ public class AppTest
         }
     }
 
+    @Test
+    public void testWhichFail() {
+        assertTrue("this test must fail", false);
+    }
+
     @After
     public void cleanUpStreams() {
         System.setOut(null);
